727458fdbf2c5b8ab9b65976cf3aec30d4156116
Susanna Huhtanen <ihmis.suski@gmail.com>
2012-02-19 17:59:42 +0200

    Toolbar stuff -needs to be cleaned a bit

--- examples/toolbar.js
+++ examples/toolbar.js
@@ -9,7 +9,7 @@ Gtk.init(null, 0);
 
 
 /*-------------------------------------------*/
-myW = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL});
+myW = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL, default_width: 100});
 myW.title = "TestForm";
 myW.connect("destroy", function(){Gtk.main_quit()});
 var grid = new Gtk.Grid({border_width:10});
@@ -20,14 +20,15 @@ var toolbar = new Gtk.Toolbar();
 toolbar.get_style_context().add_class(Gtk.STYLE_CLASS_PRIMARY_TOOLBAR);
 toolbar.set_style(Gtk.ToolbarStyle.ICONS);
 var buttonOpen = new Gtk.ToolButton.new_from_stock(Gtk.STOCK_OPEN);
+toolbar.set_hexpand(true);
 buttonOpen.connect("clicked", function() {
   //TODO: open a list of ICAO stations
   print("KLIKS!");
 });
 toolbar.insert(buttonOpen, 0)
-var label = new Gtk.Label({label: "This is window that has a toolbar"});
+//var label = new Gtk.Label({label: "This is window that has a toolbar"});
 grid.attach(toolbar,1,1,1,1);
-grid.attach_next_to(label, toolbar,3,1,1);
+//grid.attach_next_to(label, toolbar,3,1,1);
 /*-------------------------------------------*/
 myW.show_all();
 Gtk.main();
